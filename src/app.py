2a6e3e63b2086482e3a3cad423ce8f54c691ac4d
sotor <richardsoto1010@gmail.com>
2023-10-26 19:29:45 -0400

    add class for window and placeholders

--- src/app.py
+++ src/app.py
@@ -7,3 +7,15 @@ class App(tk.Tk):
         self.title("headsup")
         self.geometry("1000x600")
         self.resizable(False, False)
+        self.window = Window(self)
+
+
+class Window:
+    def __init__(self, app):
+        self.app = app
+        self.frame = tk.Frame(self.app)
+        self.frame.pack()
+        self.label = tk.Label(self.frame, text="Hello, World!")
+        self.label.pack()
+        self.button = tk.Button(self.frame, text="Quit", command=self.app.destroy)
+        self.button.pack()
