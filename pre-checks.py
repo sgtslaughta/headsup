4aeedc26a71a0f2414e64aa93a267cc38f5ed53b
richardsoto1010@gmail.com <ILovegithub21!@>
2023-10-30 11:03:47 -0400

    Fix bootstrap

--- pre-checks.py
+++ pre-checks.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
+
+import run as run
 from os import geteuid
-import lib.app as app
 import subprocess
+
+
+def check_tkinter():
+    try:
+        import tkinter
+        return True
+    except ImportError:
+        return False
+
+
+def install_tkinter():
+    try:
+        subprocess.check_call(["sudo", "apt", "update"])
+        subprocess.check_call(["sudo", "apt", "install", "python3-tk"])
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
 
 def is_xterm_installed():
@@ -36,17 +54,26 @@ def xterm_check():
         return True
 
 
-def main():
-    # root check
+def check_root():
     if geteuid() != 0:
         print("You must be root to run this program.")
         exit(1)
-    # check if xterm installed
+
+
+def run_checks():
+    check_root()
+    if not check_tkinter():
+        if install_tkinter():
+            print("Tkinter installed.")
+        else:
+            print("Failed to install tkinter. Check your package manager and "
+                  "permissions.")
+            exit(1)
     if not xterm_check():
         exit(1)
-    prog = app.App()
-    prog.mainloop()
+    print("All checks passed.")
+    run.run_app()
 
 
 if __name__ == '__main__':
-    main()
+    run_checks()
---
+++ run.py
@@ -0,0 +1,6 @@
+import lib.app as app
+
+def run_app():
+    prog = app.App()
+    prog.mainloop()
+
